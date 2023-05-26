77212d0da8902f20d16b8f0f38a53c343272a0bc
asbilim <bilimdepaulilian@gmail.com>
2023-05-26 23:26:31 +0100

    creating popular job card

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
-import { View, Text, TouchableOpacity,FlatList,ActivityIndicator } from 'react-native'
+import { View, Text} from 'react-native'
 
 import styles from './popularjobcard.style'
 
-import {useRouter} from "expo-router"
-
-import {COLORS,SIZES} from "../../../constants";
-
-import PopularJobCard  from "../../common/cards/popular/PopularJobCard"
 
 const PopularJobCard = () => {
 
-  const router = useRouter()
+
 
   return (
-    <View>
-      <Text>PopularJobCard</Text>
+    <View style={styles.container}>
+
+    
     </View>
   )
 }
